a5e56cb53ee0030af37db343665b764257f5346b
Azkalyasin <yasinadzkiaazkal@gmail.com>
2024-03-29 08:59:30 +0700

    Function input

--- quickSort/quickSort.cpp
+++ quickSort/quickSort.cpp
@@ -6,6 +6,32 @@ int arr[20];
 int cmp_count = 0;
 int mov_count = 0;
 int n;
+
+void input() {
+	while (true)
+	{
+		cout << "Masukkan banyak element pada array: ";
+		cin >> n;
+		if (n <= 20) {
+			break;
+		}
+		else
+		{
+			cout << "\nArray ddapat mempunyai maksimal 20 " << endl;
+		}
+	}
+
+	cout << endl;
+	cout << "\n======================" << endl;
+	cout << "\nMasukkan element Array" << endl;
+	cout << "\n======================" << endl;
+
+	for (int i = 0; i < n; i++) {
+		cout << "<" << (i + 1) << ">";
+		cin >> arr[i];
+	}
+}
+
 int main()
 {
 
